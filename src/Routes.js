e6b567f5e7aaa0063432e09cd820c8e3ef528757
Marlon <marlon.englemam@gmail.com>
2020-04-11 15:29:05 -0300

    Added(react navigation): library for navigation between screens added

---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+import Login from './screens/login/Login';
+import Home from './screens/home/Home';
+
+
+const Drawer = createDrawerNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Login">
+                <Drawer.Screen name="Login" component={Login} />
+                <Drawer.Screen name="Home" component={Home} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+}

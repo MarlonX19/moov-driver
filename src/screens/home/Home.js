5880ebbf8a6897b94bf63fc9e8acc2906fa6c8eb
marlonx19 <marlon.englemam@gmail.com>
2020-09-08 02:02:11 -0300

    improvements: accepting requests for delivery

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,15 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useContext } from 'react';
 import { View, Text, Animated, TouchableOpacity, Image } from 'react-native';
+import { showMessage } from "react-native-flash-message";
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import MapView from 'react-native-maps';
 import io from 'socket.io-client';
 
 import Header from '../../components/Header';
+
+import AuthContext from '../../contexts/auth';
+
+import { api } from '../../services/auth';
 
 import styles from './styles';
 
@@ -17,6 +22,8 @@ export default function Home(props) {
   const [newDel, setNewDel] = useState(false);
   const [socket, setSocket] = useState(null);
   const [clientData, setClientData] = useState(null);
+
+  const { user } = useContext(AuthContext);
 
   const fadeIn = () => {
     // Will change fadeAnim value to 1 in 5 seconds
@@ -79,13 +86,47 @@ export default function Home(props) {
 
   }, [socket])
 
-  useEffect(()=>{
-    if(newDel){
+  useEffect(() => {
+    if (newDel) {
       fadeIn2()
       goUp2()
     }
 
   }, [newDel])
+
+
+  async function handleAccept(){
+    const response  = await api.post('/delivery' , {
+      accepted: true,
+      delivered: false,
+      value: clientData?.value,
+      observation: clientData.observation,
+      fromLatitude: clientData.fromLatitude,
+      fromLongitude: clientData.fromLongitude, 
+      toLatitude: clientData.toLatitude,
+      toLongitude: clientData.toLongitude,
+      fromTown: clientData.fromTown,
+      toTown: clientData.toTown,
+      delivered_at: null,
+      date: clientData.date,
+      driver_id: 1,
+      user_id: clientData.user.id,
+    })
+
+    if(response?.data?.messageCode === '200') {
+      showMessage({
+        message: "Parabéns, agora basta entregar!",
+        type: "success",
+      });
+      setNewDel(false)
+    } else {
+      showMessage({
+        message: "Entrega não mais disponível",
+        type: "info",
+      });
+      setNewDel(false)
+    }
+  }
 
 
   return (
@@ -145,18 +186,18 @@ export default function Home(props) {
         <View>
           <Text style={styles.welcomeText}>Pedido de entrega!</Text>
         </View>
-        <Image source={{ uri: `http://192.168.15.13:3000/files/${clientData?.avatar_path}` }} style={{ width: 70, height: 70, borderRadius: 30, borderWidth: 0.5, borderColor: 'purple' }} />
-        <Text style={{ fontSize: 18, color: '#333'}}>{`${clientData?.first_name + ' ' + clientData?.last_name}`}</Text>
+        <Image source={{ uri: `http://192.168.15.13:3000/files/${clientData?.user?.avatar_path}` }} style={{ width: 70, height: 70, borderRadius: 30, borderWidth: 0.5, borderColor: 'purple' }} />
+        <Text style={{ fontSize: 18, color: '#333' }}>{`${clientData?.user?.first_name + ' ' + clientData?.user?.last_name}`}</Text>
         <View style={styles.cardValue}>
-          <Text style={styles.value}>R$150</Text>
+          <Text style={styles.value}>R${clientData?.value}</Text>
           <Icon name="money-bill-wave" size={32} color="lightgreen" />
         </View>
         <View style={{ width: '100%', paddingLeft: 30 }}>
           <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Icon name="map-pin" size={20} color="lightgreen" /><Text style={{ paddingLeft: 10, color: 'grey', fontSize: 18, marginVertical: 3 }}>De: Araras - São Paulo</Text>
+            <Icon name="map-pin" size={20} color="lightgreen" /><Text style={{ paddingLeft: 10, color: 'grey', fontSize: 14, marginVertical: 3 }}>De: {clientData?.fromTown}</Text>
           </View>
           <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Icon name="map-pin" size={20} color="red" /><Text style={{ paddingLeft: 10, color: 'grey', fontSize: 18, marginVertical: 3 }}>Para: Leme - Rio de Janeiro</Text>
+            <Icon name="map-pin" size={20} color="red" /><Text style={{ paddingLeft: 10, color: 'grey', fontSize: 14, marginVertical: 3 }}>Para: {clientData?.toTown}</Text>
           </View>
         </View>
         <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-around' }}>
@@ -167,7 +208,7 @@ export default function Home(props) {
             <Text style={styles.btnText}>NEGAR</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => { }}
+            onPress={() => handleAccept()}
             style={styles.acceptBtn}
           >
             <Text style={styles.btnText}>ACEITAR</Text>
